44ea58513a96220877414bf2767461e37f9497b7
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2020-09-22 10:40:54 -0400

    test2

--- bin/estP1D-FFT.py
+++ bin/estP1D-FFT.py
@@ -68,10 +68,10 @@ if __name__ == '__main__':
 
         p1d_f = np.abs(np.fft.rfft(delta_f) * dv)**2 / (dv*delta_f.size)
         this_k_arr = 2*np.pi*np.fft.rfftfreq(delta_f.size, dv)
-        # p1d_f /= getSpectographWindow2(this_k_arr, bq.specres, bq.dv)
+        p1d_f /= getSpectographWindow2(this_k_arr, bq.specres, bq.dv)
 
         # ignore k=0 mode
-        p, c = binPowerSpectra(this_k_arr[1:], p1d_f[1:], config_qmle.k_edges)
+        p, c = binPowerSpectra(this_k_arr, p1d_f, config_qmle.k_edges)
         
         power[z_bin_no] += p
         counts[z_bin_no] += c[1:-1]
