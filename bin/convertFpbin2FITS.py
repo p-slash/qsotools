6c30917d9594edfaf63a2e6ee689d649b2dee227
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2021-01-28 11:53:06 -0500

    fitsio fix: rw

--- bin/convertFpbin2FITS.py
+++ bin/convertFpbin2FITS.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     dtype_fp = np.dtype([('fisher', np.double, (N,N)), ('power', np.double, N)])
     data = np.zeros(1, dtype=dtype_fp)
 
-    fits = fitsio.FITS(ospath_join(config_qmle.qso_dir), "combined_Fp.fits.gz")
+    fits = fitsio.FITS(ospath_join(config_qmle.qso_dir,"combined_Fp.fits.gz"), 'rw')
 
     for f in qso_filename_list:
         fisher, power = readFPBinFile(f)
