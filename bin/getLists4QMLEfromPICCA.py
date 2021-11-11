56ad76c0c34ad36c27d24e341f0cb2e91f8bec2d
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2021-11-11 16:40:27 -0500

    ospath with directory

--- bin/getLists4QMLEfromPICCA.py
+++ bin/getLists4QMLEfromPICCA.py
@@ -75,7 +75,7 @@ class GetNCopy(object):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("Directory", help="Directory.")
-    parser.add_argument("--flist", nargs='*', help="Only convert these delta files. Cancels directory list.")
+    parser.add_argument("--flist", nargs='*', help="Only convert these delta files in Directory.")
     parser.add_argument("--snr-cut", help="S/N cut using MEANSNR in header.", default=0, type=float)
     parser.add_argument("--oversample-rmat", type=int, default=1, 
         help="Oversampling factor for resolution matrix. "\
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         os_makedirs(args.osamp_dir, exist_ok=True)
 
     if args.flist:
-        all_deltas = args.flist
+        all_deltas = [ospath_join(args.Directory, x) for x in args.flist]
     else:
         all_deltas = glob.iglob(ospath_join(args.Directory, "delta-*.fits*"))
     all_slst = set()
