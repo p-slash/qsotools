c8d9460025f4fbc25161e2d152d9fefe6936b094
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2021-12-01 13:26:51 -0500

    turn off zscore masking to prevent mean flux skewing

--- bin/reduceQQMocks.py
+++ bin/reduceQQMocks.py
@@ -136,8 +136,9 @@ class Reducer(object):
             if args.compute_mean_flux:
                 try:
                     qso = Spectrum(wave, flux, 1./np.sqrt(ivar), z_qso, 5000, 1.0, {'RA':ra, 'DEC':dec})
-                    qso.setZScoreMask(fsigma=1, esigma=3.5)
-                    qso.applyMask()
+                    # zscore masking skews the resulting mean flux
+                    # qso.setZScoreMask(fsigma=1, esigma=3.5)
+                    # qso.applyMask()
                     self.local_meanflux_hist.addSpectrum(qso, weight=1)
                 except Exception as e:
                     print(e)
