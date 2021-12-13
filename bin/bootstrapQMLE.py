9738d2ebc23aa4851b124d35c0d2b6bcd69f5e15
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2021-12-13 17:08:01 -0500

    removed multiprocessing (it does not help much because of matrix multiplications). Now generating one big array for bootstrapping, instead of each for each file. (Generating bootstrap realizations by file, constains the fluctuations.)

--- bin/bootstrapQMLE.py
+++ bin/bootstrapQMLE.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
+
+# Multiprocessing does not help speed up computation
+# Generating bootstrap realizations by file, constains the fluctuations
+# Generate one big boot array, files get their parts
+
 import argparse
 import sys
 from os.path import join as ospath_join, getsize as ospath_getsize
-from multiprocessing import Pool
-from itertools import groupby
 import glob
 import struct
 import time
 import logging
 
 import numpy as np
-import fitsio
 
-def getNfromBootfile(fname):
+def getNumbersfromBootfile(fname):
+    filesize = ospath_getsize(fname)
+
     with open(fname, "rb") as f:
         N = int(struct.unpack('i', f.read(struct.calcsize('i')))[0])
-    return N
+
+        id_size     = struct.calcsize('i')
+        fisher_size = struct.calcsize('d'*N*N)
+        power_size  = struct.calcsize('d'*N)
+        aspec_size  = id_size+fisher_size+power_size
+        nspec = int((filesize-struct.calcsize('i'))/aspec_size)
+
+    return N, nspec
 
 # Returns fishers, powers
 # fishers.shape = (nspec, N*N)
@@ -69,67 +80,13 @@ class Progress(object):
             etime = (time.time()-self.start_time)/60 # min
             logging.info(f"Progress: {curr_progress}%. Elapsed time {etime:.1f} mins.")
             self.last_progress = curr_progress
-
-
-class Booter(object):
-    """docstring for Booter"""
-    def __init__(self, N, args):
-        self.args = args
-        self.Nbin = N
-
-    def __call__(self, pe):
-        fname = ospath_join(self.args.BootDirectory, f"bootresults-{pe}.dat")
-        # t0 = time.time()
-
-        # Read fishers and powers
-        fishers, powers = readBootFile(fname)
-
-        # t1 = time.time()
-        # etime = (t1-t0)/60 # min
-        # logging.info(f"Reading bootresults file. Elapsed time: {etime:.1f} mins.")
-        # t0 = t1
-
-        nspec = powers.shape[0]
-        assert (powers.shape[1] == self.Nbin)
-
-        # Create zeros for bootstrap results
-        this_fisher   = np.zeros((self.args.bootnum, self.Nbin*self.Nbin))
-        this_power_b4 = np.zeros((self.args.bootnum, self.Nbin))
-
-        # t1 = time.time()
-        # etime = (t1-t0)/60 # min
-        # logging.info(f"Creating empty arrays. Elapsed time: {etime:.1f} mins.")
-        # t0 = t1
-
-        # Each file has a different seed for multiprocessing
-        RND = np.random.default_rng(self.args.seed + pe)
-        booted_indices = RND.integers(low=0, high=nspec, size=(self.args.bootnum, nspec))
-
-        # t1 = time.time()
-        # etime = (t1-t0)/60 # min
-        # logging.info(f"Generated boot indices. Elapsed time: {etime:.1f} mins.")
-        # t0 = t1
-
-        counts = getCounts(booted_indices, self.args.bootnum, nspec)
-
-        # t1 = time.time()
-        # etime = (t1-t0)/60 # min
-        # logging.info(f"Getting counts. Elapsed time: {etime:.1f} mins.")
-        # t0 = t1
-
-        this_fisher   = counts @ fishers
-        this_power_b4 = counts @ powers
-
-        return this_fisher, this_power_b4
         
 if __name__ == '__main__':
     # Arguments passed to run the script
     parser = argparse.ArgumentParser()
     parser.add_argument("BootDirectory", help="Directory where bootresults-N.dat are present.")
-    parser.add_argument("OutputFile", help="Output file relative to BootDirectory.")
     parser.add_argument("--bootnum", default=1000, type=int, \
         help="Number of bootstrap resamples. Default: %(default)s")
-    parser.add_argument("--nproc", type=int, default=1)
     parser.add_argument("--seed", default=3422, type=int)
     parser.add_argument("--save-cov", action="store_true")
     args = parser.parse_args()
@@ -142,22 +99,50 @@ if __name__ == '__main__':
     # Find the number of bootstrapping files
     bootfiles = glob.glob(ospath_join(args.BootDirectory, "bootresults-*.dat"))
     nbootfiles = len(bootfiles)
+
     logging.info("There are %d bootresults files.", nbootfiles)
 
+    # Generating bootstrap realizations by file, constains the fluctuations
+    # Generate one big boot array, each file gets their parts
+    # We will need indices for each file
+    nspec_total = 0
+    indices     = np.empty(nbootfiles+1)
+    Nbins       = getNfromBootfile(bootfiles[0])
+
+    indices[0]  = 0
+    for pe in range(nbootfiles):
+        fname     = ospath_join(args.BootDirectory, f"bootresults-{pe}.dat")
+        N1, nspec = getNumbersfromBootfile(fname)
+
+        assert (N1 == Nbins)
+
+        nspec_total  += nspec
+        indices[pe+1] = nspec_total
+
+    logging.info("There are %d spectra.", nspec_total)
+
+    # Generate bootstrap realizations through indexes
+    RND            = np.random.default_rng(args.seed)
+    booted_indices = RND.integers(low=0, high=nspec_total, size=(args.bootnum, nspec_total))
+    boot_counts    = getCounts(booted_indices, args.bootnum, nspec_total)
+    logging.info(f"Generated boot indices.")
+
+    # Allocate memory for matrices
+    total_fisher   = np.zeros((args.bootnum, Nbins*Nbins))
+    total_power_b4 = np.zeros((args.bootnum, Nbins))
+    total_power    = np.zeros((args.bootnum, Nbins))
+
+    # Set up progress tracker
     pcounter = Progress(nbootfiles)
-    N = getNfromBootfile(bootfiles[0])
-    total_fisher   = np.zeros((args.bootnum, N*N))
-    total_power_b4 = np.zeros((args.bootnum, N))
-    total_power    = np.zeros((args.bootnum, N))
+    for pe in range(nbootfiles):
+        fname     = ospath_join(args.BootDirectory, f"bootresults-{pe}.dat")
 
-    with Pool(processes=args.nproc) as pool:
-        imap_it = pool.imap(Booter(N, args), range(nbootfiles))
+        # Read fishers and powers
+        fishers, powers = readBootFile(fname)
+        this_counts     = boot_counts[:, indices[pe]:indices[pe+1]]
 
-        for (fisher1, power1) in imap_it:
-            total_fisher   += fisher1
-            total_power_b4 += power1
-
-            pcounter.increase()
+        total_fisher   += this_counts @ fishers
+        total_power_b4 += this_counts @ powers
 
     logging.info("Calculating bootstrapped inverse Fisher and power...")
     for bi in range(args.bootnum):
@@ -165,7 +150,8 @@ if __name__ == '__main__':
 
     # Save power to a file
     # Set up output file
-    output_fname = ospath_join(args.BootDirectory, args.OutputFile)
+    output_fname = ospath_join(args.BootDirectory, 
+        "bootstrap-power-n%d-s%d.txt" % (args.bootnum, args.seed))
     np.savetxt(output_fname, total_power)
     logging.info("Power saved as ", output_fname)
 
@@ -173,7 +159,8 @@ if __name__ == '__main__':
     # when save-cov passed
     if args.save_cov:
         bootstrap_cov = np.cov(total_power, rowvar=False)
-        cov_filename = oospath_join(args.BootDirectory, f"cov-{args.OutputFile}")
-        np.savetxt(cov_filename, bootstrap_cov)
-        logging.info("Covariance saved as ", cov_filename)
+        output_fname = ospath_join(args.BootDirectory, 
+            "bootstrap-cov-n%d-s%d.txt" % (args.bootnum, args.seed))
+        np.savetxt(output_fname, bootstrap_cov)
+        logging.info("Covariance saved as ", output_fname)
 
