numerical stability and fix

py/qsotools/metals.py
@@ -37,30 +37,51 @@ class AbsorptionProfile(object):
     me = 9.109384e-28 # g
     c_cms = fid.LIGHT_SPEED * 1e5 # km to cm for c
     aij_coeff = np.pi * qe**2 / me / c_cms # cm^2 s^-1
-    
-    # There could be a missing 2 compared to Tie 2022
-    def _getOpticalDepth(nu, lambda12_A, log10N, b, f12):
-        a12 = AbsorptionProfile.aij_coeff * f12
-        nu12 = fid.LIGHT_SPEED*1e13/lambda12_A
-        gamma = 2 * nu12**2 / AbsorptionProfile.c_cms**2 * a12
-        
-        sigma_gauss = nu12 * b / fid.LIGHT_SPEED / np.sqrt(2)
-        vfnc = Voigt1D(nu-nu12, sigma_gauss, gamma)
 
-        tau = 10**log10N * (a12/nu12) * vfnc * nu
+    # There could be a missing 2 compared to Tie 2022
+    # def _getOpticalDepth(nu, lambda12_A, log10N, b, f12):
+    #     a12 = AbsorptionProfile.aij_coeff * f12
+    #     nu12 = fid.LIGHT_SPEED*1e13/lambda12_A
+    #     gamma = 2 * nu12**2 / AbsorptionProfile.c_cms**2 * a12
+        
+    #     sigma_gauss = nu12 * b / fid.LIGHT_SPEED / np.sqrt(2)
+    #     vfnc = Voigt1D(nu-nu12, sigma_gauss, gamma)
+
+    #     tau = 10**log10N * (a12/nu12) * vfnc * nu
+
+    #     return tau
+    # def getFlux(wave, ion, log10N, b):
+    #     nu = fid.LIGHT_SPEED * 1e13 / wave
+
+    #     if isinstance(wave, np.ndarray):
+    #         tau = np.zeros_like(wave)
+    #     else:
+    #         tau = 0
+
+    #     for transition in IonTransitions[ion]:
+    #         tau += AbsorptionProfile._getOpticalDepth(nu, transition.wave, log10N, b, transition.f_osc)
+
+    #     return np.exp(-tau)
+
+    def _getOpticalDepth(wave_A, lambda12_A, log10N, b, f12):
+        a12 = AbsorptionProfile.aij_coeff * f12
+        gamma = (2e3 / fid.LIGHT_SPEED) * a12 / lambda12_A
+        
+        sigma_gauss = b / fid.LIGHT_SPEED / np.sqrt(2)
+        vfnc = Voigt1D(lambda12_A/wave_A - 1, sigma_gauss, gamma)
+
+        tau = a12 * 10**(log10N-13) * (lambda12_A/fid.LIGHT_SPEED) * vfnc * (lambda12_A/wave_A)
 
         return tau
 
     def getFlux(wave, ion, log10N, b):
-        nu = fid.LIGHT_SPEED * 1e13 / wave
-
         if isinstance(wave, np.ndarray):
             tau = np.zeros_like(wave)
         else:
             tau = 0
 
         for transition in IonTransitions[ion]:
-            tau += AbsorptionProfile._getOpticalDepth(nu, transition.wave, log10N, b, transition.f_osc)
+            tau += AbsorptionProfile._getOpticalDepth(wave, transition.wave, log10N, b, transition.f_osc)
 
         return np.exp(-tau)
     
@@ -73,8 +94,7 @@ class AbsorptionProfile(object):
 
         for i in range(ntransitions):
             l12 = transitions[i].wave
-            nu12 = fid.LIGHT_SPEED * 1e13 / l12
-            tau = AbsorptionProfile._getOpticalDepth(nu12, l12, log10N, b, transitions[i].f_osc)
+            tau = AbsorptionProfile._getOpticalDepth(l12, l12, log10N, b, transitions[i].f_osc)
             maximafluxes[i] = 1-np.exp(-tau)
 
         maxheight = np.max(maximafluxes)
@@ -90,10 +110,9 @@ class AbsorptionProfile(object):
             f12 = transition.f_osc
 
             def oneminusflux(l):
-                nu = fid.LIGHT_SPEED * 1e13 / l
-                return 1-np.exp(-AbsorptionProfile._getOpticalDepth(nu, l12, log10N, b, f12))
+                return 1-np.exp(-AbsorptionProfile._getOpticalDepth(l, l12, log10N, b, f12))
 
-            ew = quad(oneminusflux, l12-100, l12+100, epsabs=1e-8, epsrel=1e-08, limit=500)[0]
+            ew = quad(oneminusflux, l12-20, l12+20, epsabs=1e-8, epsrel=1e-6, limit=5000, points=l12)[0]
 
             if unit == 'kms':
                 ew *= fid.LIGHT_SPEED/l12