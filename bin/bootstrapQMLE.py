88e28377147d7127cc8d796da78a233451999b5b
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2022-02-25 10:32:38 -0500

    import Progress from utils module

--- bin/bootstrapQMLE.py
+++ bin/bootstrapQMLE.py
@@ -14,6 +14,8 @@ import logging
 
 import numpy as np
 
+from qsotools.utils import Progress
+
 def getNumbersfromBootfile(fname):
     filesize = ospath_getsize(fname)
 
@@ -51,25 +53,6 @@ def getCounts(booted_indices):
         counts[b] = np.bincount(booted_indices[b], minlength=no_spectra)
 
     return counts
-
-class Progress(object):
-    """docstring for Progress"""
-    def __init__(self, total, percThres=5):
-        self.i = 0
-        self.total = total
-        self.percThres = percThres
-        self.last_progress = 0
-        self.start_time = time.time()
-
-    def increase(self):
-        self.i+=1
-        curr_progress = int(100*self.i/self.total)
-        print_condition = (curr_progress-self.last_progress >= self.percThres) or (self.i == 0)
-
-        if print_condition:
-            etime = (time.time()-self.start_time)/60 # min
-            logging.info(f"Progress: {curr_progress}%. Elapsed time {etime:.1f} mins.")
-            self.last_progress = curr_progress
         
 if __name__ == '__main__':
     # Arguments passed to run the script
