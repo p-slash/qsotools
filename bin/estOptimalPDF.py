072072299e3bf1c2b3a203a7684594493b4e4c31
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2022-11-22 19:38:49 -0500

    sparse matrix test

--- bin/estOptimalPDF.py
+++ bin/estOptimalPDF.py
@@ -4,6 +4,7 @@ import logging
 
 import numpy as np
 import cupy
+import cupyx
 from astropy.table import Table
 from mpi4py import MPI
 
@@ -31,11 +32,11 @@ class cpInterp2d(object):
         y_g[y_g>=self.y_gpu[-1]] = self.y_gpu[-1]-1e-6
 
         xx = (x_g-self.x_gpu[0])/self.dx
-        xi = (xx).astype(np.int64)
+        xi = (xx).astype(int)
         dxi = xx - xi
 
         yy = (y_g-self.y_gpu[0])/self.dy
-        yi = (yy).astype(np.int64)
+        yi = (yy).astype(int)
         dyi = yy - yi
 
         result = self.f_gpu[xi, yi] * (1-dxi) * (1-dyi)\
@@ -94,6 +95,28 @@ class PDFEstimator(object):
 
         return cinv_gpu
 
+    def constructBinMat(self, flux_idx_gpu):
+        w = (flux_idx_gpu>0) & (flux_idx_gpu<self.nfbins+1)
+        ndata = cupy.sum(w)
+        data = cupy.ones(ndata, dtype=float)
+        indices = cupy.empty(ndata, dtype=int)
+
+        ubins, bidx, bcts = cupy.unique(flux_idx_gpu, return_inverse=True, return_counts=True)
+        lastcol = min(ubins[-1], nfbins)
+        indptr = cupy.zeros(lastcol+1, dtype=int)
+        jj=0
+        for ii in range(1, lastcol+1):
+            if (ubins[jj] != ii):
+                indptr[ii]=indptr[ii-1]
+            else:
+                indptr[ii]=indptr[ii-1]+bcts[jj]
+                indices[indptr[ii-1]:indptr[ii]] = cupy.nonzero(bidx==jj)
+                jj += 1
+
+        Bmat = cupyx.scipy.sparse.csc_matrix((data, indices, indptr),
+            shape=(flux_idx_gpu, nfbins))
+        return Bmat
+
     def getEstimates(self, qso):
         if self.args.smooth_noise_sigmaA > 0:
             qso.smoothNoise(sigma_A=self.args.smooth_noise_sigmaA)
@@ -118,19 +141,22 @@ class PDFEstimator(object):
 
         cinv_gpu = self.getInvCovariance(w_gpu, z_gpu, e_gpu)
         flux_idx_gpu = cupy.searchsorted(self.flux_edges_gpu, f_gpu)
+        Bmat = self.constructBinMat(flux_idx_gpu)
+        BmatT = Bmat.transpose()
 
         y = cinv_gpu.dot(f_gpu)
         i1 = z_bin_no * self.nfbins
         i2 = i1 + self.nfbins
-        self.flux_pdf[i1:i2] += cupy.bincount(flux_idx_gpu, weights=y, minlength=self.nfbins+2)[1:-1]
+        # self.flux_pdf[i1:i2] += cupy.bincount(flux_idx_gpu, weights=y, minlength=self.nfbins+2)[1:-1]
+        self.flux_pdf[i1:i2] += BmatT.dot(y)
 
-        _2d_bin_idx = cupy.ravel(
-            flux_idx_gpu[:, cupy.newaxis]
-            + (self.nfbins+2) * flux_idx_gpu[cupy.newaxis, :]
-        )
-        temp_cinv = cupy.bincount(_2d_bin_idx, weights=cinv_gpu.ravel(), minlength=(self.nfbins+2)**2)
-        temp_cinv = temp_cinv.reshape(self.nfbins+2, self.nfbins+2)[1:-1, 1:-1]
-        self.fisher[i1:i2, i1:i2] += temp_cinv
+        # _2d_bin_idx = cupy.ravel(
+        #     flux_idx_gpu[:, cupy.newaxis]
+        #     + (self.nfbins+2) * flux_idx_gpu[cupy.newaxis, :]
+        # )
+        # temp_cinv = cupy.bincount(_2d_bin_idx, weights=cinv_gpu.ravel(), minlength=(self.nfbins+2)**2)
+        # temp_cinv = temp_cinv.reshape(self.nfbins+2, self.nfbins+2)[1:-1, 1:-1]
+        self.fisher[i1:i2, i1:i2] += BmatT @ cinv @ Bmat
 
     def __call__(self, fname):
         if self.config_qmle.picca_input:
