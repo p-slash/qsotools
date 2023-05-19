8ec960e334125cdb97ebf281a3fcfd344c0c2e23
Naim Goksel Karacayli <ngokselk@gmail.com>
2023-05-19 08:27:37 -0400

    tqdm package for dynamic progress bar. block jackknife for speed

--- py/qsotools/scripts/jackknifeQMLEChunk.py
+++ py/qsotools/scripts/jackknifeQMLEChunk.py
@@ -12,7 +12,7 @@ from numba import njit
 from scipy.linalg import cho_factor, cho_solve
 import fitsio
 
-from qsotools.utils import Progress
+from tqdm import tqdm
 
 
 @njit("f8[:, :](i8, f8[:])")
@@ -34,7 +34,7 @@ class Chunk():
         self.ndim = ndim
         self.istart = istart
         self.pk = data[:ndim] - data[ndim:2 * ndim] - data[2 * ndim:3 * ndim]
-        self.upper_fisher = data[3 * ndim]
+        self.upper_fisher = data[3 * ndim:]
 
     def form_square_fisher(self):
         return _fast_construct_fisher(self.ndim, self.upper_fisher)
@@ -86,12 +86,28 @@ def readfile_calcpsfisher(X):
     return chunks, power, fisher
 
 
-def one_jackknife_est(chunk, total_power, total_fisher):
+def one_jackknife_est(X):
+    chunk, total_power, total_fisher, di = X
     xpower = total_power.copy()
     xfisher = total_fisher.copy()
 
     xpower = chunk.add_to_total_power(xpower, m=-1)
     xfisher = chunk.add_to_total_fisher(xfisher, m=-1)
+    xfisher[di] = np.where(xfisher[di] == 0, 1, xfisher[di])
+    c, low = cho_factor(xfisher)
+    xpower = cho_solve((c, low), xpower)
+    return xpower
+
+
+def block_jackknife_est(X):
+    chunks, total_power, total_fisher, di = X
+    xpower = total_power.copy()
+    xfisher = total_fisher.copy()
+
+    for chunk in chunks:
+        xpower = chunk.add_to_total_power(xpower, m=-1)
+        xfisher = chunk.add_to_total_fisher(xfisher, m=-1)
+    xfisher[di] = np.where(xfisher[di] == 0, 1, xfisher[di])
     c, low = cho_factor(xfisher)
     xpower = cho_solve((c, low), xpower)
     return xpower
@@ -105,12 +121,14 @@ def main():
         "BootChunkFileBase", help="BootChunkFile as described in QMLE.")
     parser.add_argument("Nk", help="Number of k bins", type=int)
     parser.add_argument("Nz", help="Number of z bins", type=int)
+    parser.add_argument(
+        "--nblocks", help="Use block jackknife instead.", type=int)
     # parser.add_argument("--save-powers", action="store_true")
     # parser.add_argument("--calculate-originals", action="store_true")
     parser.add_argument("--fbase", default="")
     parser.add_argument("--nproc", type=int, default=None)
     args = parser.parse_args()
-    logging.basicConfig()
+    logging.basicConfig(level=logging.DEBUG)
 
     outdir = ospath_dir(args.BootChunkFileBase)
     if outdir == "":
@@ -132,16 +150,34 @@ def main():
     imap_it = pool.imap(readfile_calcpsfisher, args_list)
 
     logging.info("Reading chunk files and calculating total power & fisher...")
-    pcounter = Progress(len(args_list))
-    for chunks, power, fisher in imap_it:
+    for chunks, power, fisher in tqdm(imap_it):
         all_chunks.extend(chunks)
         total_power += power
         total_fisher += fisher
-        pcounter.increase()
+
+    nchunks = len(all_chunks)
+    if args.nblocks and args.nblocks > 1 and args.nblocks < nchunks / 10:
+        indices = np.linspace(0, nchunks, args.nblocks + 1).astype(int)
+        all_chunks = [
+            all_chunks[indices[_]:indices[_ + 1]]
+            for _ in range(args.nblocks)
+        ]
+
+        jackknife_method = block_jackknife_est
+    else:
+        jackknife_method = one_jackknife_est
 
     logging.info("Calculating all jackknife estimates...")
-    args_list = [(chunk, total_power, total_fisher) for chunk in all_chunks]
-    all_powers = pool.starmap(one_jackknife_est, args_list)
+    di = np.diag_indices(total_power.size)
+    args_list = [
+        (chunk, total_power, total_fisher, di)
+        for chunk in all_chunks
+    ]
+    all_powers = []
+    imap_it = pool.imap(jackknife_method, args_list)
+    for xpower in tqdm(imap_it):
+        all_powers.append(xpower)
+
     logging.info("Done.")
     pool.close()
 
